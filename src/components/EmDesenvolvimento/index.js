e4150252f80ac2b42813e324ca21e1b7f3fa4c02
LionyxML <rahul.juliato@gmail.com>
2021-10-01 21:28:48 -0300

    Melhorado o texto do emDesenvolvimento, consertado link da Subestacao

--- src/components/EmDesenvolvimento/index.js
+++ src/components/EmDesenvolvimento/index.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Alert } from "react-bootstrap";
 
 const EmDesenvolvimento = () => {
   return (
-    <Container className="mt-3">
-      <h2>Que pena!</h2>
+    <Alert className="mt-3 w-50 mx-auto mt-5" variant="success">
+      <Alert.Heading>Ahh... que pena!</Alert.Heading>
+      <hr />
       <p>Você tentou acessar uma página que ainda está em desenvolvimento...</p>
       <p>Volte em breve!</p>
-    </Container>
+    </Alert>
   );
 };
 
--- src/components/Subestacao/index.js
+++ src/components/Subestacao/index.js
@@ -4,7 +4,7 @@ import Iframe from "react-iframe";
 export default function Subestacao() {
   return (
     <Iframe
-      url="http://ec2-18-117-132-1.us-east-2.compute.amazonaws.com:1880/ui/#!/3"
+      url="http://ec2-18-117-132-1.us-east-2.compute.amazonaws.com:1880/ui/#!/2"
       width="100%"
       height="100%"
       id="myId"
