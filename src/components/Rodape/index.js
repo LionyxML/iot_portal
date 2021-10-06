f0044a4f8349f2d370033afee0deb0816c0a6330
LionyxML <rahul.juliato@gmail.com>
2021-10-06 16:43:33 -0300

    Consertado Rodapé

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -4,10 +4,7 @@ import "./index.css";
 export const Rodape = () => {
   return (
     <div className="rodape">
-      <p>
-        Instituto SENAI de Tecnologia em Energia "Com. Santoro Mirone" -
-        Indaiatbuba &copy;2021
-      </p>
+      <p>Indaiatuba - &copy; RMJ 2021</p>
     </div>
   );
 };
